2122a7e7f6cb0f4e2786f25a2663a5c28ef27dce
tosh-m12 <tosh_m12@yahoo.co.jp>
2025-01-25 22:25:56 +0800

    指摘１修正管理画面検索機能

--- tabelog/admin.py
+++ tabelog/admin.py
@@ -5,9 +5,19 @@ from django.utils.safestring import mark_safe
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    search_fields = ['name']
 
-admin.site.register(CustomUser)
-admin.site.register(Shop)
+
+class ShopAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Shop, ShopAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review)
 admin.site.register(Booking)
